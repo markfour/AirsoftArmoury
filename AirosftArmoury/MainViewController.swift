15b294945bdfdfb84524a884868c995979fba568
markfour <axhfaro@gmail.com>
2018-10-30 14:00:39 +0900

    Implement tableView

--- AirosftArmoury/MainViewController.swift
+++ AirosftArmoury/MainViewController.swift
@@ -7,11 +7,33 @@ import UIKit
 
 class MainViewController: UIViewController {
   let tableView = UITableView(frame: CGRect.zero, style: .plain)
+  
+  var contents: [String] = [];
 
   override func viewDidLoad() {
     super.viewDidLoad()
     
-//    tableView.delegate = self
-//    tableView.dataSource = self
+    tableView.delegate = self
+    tableView.dataSource = self
+  }
+}
+
+extension MainViewController: UITableViewDataSource {
+  func numberOfSections(in tableView: UITableView) -> Int {
+    return 1
+  }
+  
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return contents.count
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    return UITableViewCell()
+  }
+}
+
+extension MainViewController: UITableViewDelegate {
+  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    tableView.deselectRow(at: indexPath, animated: true)
   }
 }
