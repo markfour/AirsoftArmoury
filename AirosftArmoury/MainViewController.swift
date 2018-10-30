ed6e6f4497580bcb92a12a4b92700c78c2709dd2
markfour <axhfaro@gmail.com>
2018-10-30 13:50:56 +0900

    Add MainViewController

---
+++ AirosftArmoury/MainViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MainViewController.swift
+//  AirosftArmoury
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+  let tableView = UITableView(frame: CGRect.zero, style: .plain)
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+//    tableView.delegate = self
+//    tableView.dataSource = self
+  }
+}
